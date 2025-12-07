6fe642bf8bc07a708fa916b57a2ede66ea46df4c
A1B2C3-max <arymansingh05@gmail.com>
2025-12-08 00:43:02 +0530

    shorten keyword

--- new.js
+++ new.js
@@ -1,5 +1,5 @@
-let a = Number(prompt("Enter first number:"));
-let b = Number(prompt("Enter second number:"));
+let a = Number(prompt("Enter fis number:"));
+let b = Number(prompt("Enter sec number:"));
 
 let add = a + b;
 

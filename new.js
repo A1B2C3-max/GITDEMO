68e5407041384f40b7fe681b35dde8a4ddf5c546
A1B2C3-max <arymansingh05@gmail.com>
2025-12-07 23:43:33 +0530

    add initial files

---
+++ new.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("Enter first number:"));
+let b = Number(prompt("Enter second number:"));
+
+let sum = a + b;
+
+console.log("The sum is: " + sum);
+alert("The sum is: " + sum);
